611b0448ef964705837590f98155502405356f07
sansengu <sansengu@in.ibm.com>
2017-02-20 17:08:23 +0530

    My first attempt

---
+++ helloworld.go
@@ -0,0 +1,40 @@
+package main
+import "fmt"
+func main() {
+
+fmt.Println("Hello World ping pong")
+var temp1 string = "SANDIPAN"
+name := "SANDIPAN"
+//Var temp float64 = 35.01
+const pi float64 = 3.1415
+
+Sal := 100
+
+
+fmt.Println(temp1)
+
+fmt.Printf("%T \n",name)
+//fmt.Printf("%T \n",temp)
+//fmt.Printf("%T \n",pi) 
+fmt.Printf("%T \n",Sal)
+fmt.Println(Sal)
+
+for i:=1; i<5; i++ {
+
+fmt.Println(i)
+
+
+}
+
+todaysTemp := 29
+if todaysTemp <30{
+
+fmt.Println("weather is pleasant") 
+} else {fmt.Println("weather is extreme")}
+
+
+
+
+
+
+}
